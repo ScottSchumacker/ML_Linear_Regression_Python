43db6410e8fe3ec8f3d905115337835765104f02
Scott Schumacker <scottschu97@gmail.com>
2025-02-22 09:59:53 -0500

    Updated code to visualize data

--- model_script.py
+++ model_script.py
@@ -1,7 +1,17 @@
 # Scott Schumacker
+# Script to create linear regression model
+
 # Importing libaries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
+
+# Showing description of our data set
+print(salary_data.describe())
+
+plt.figure(figsize = (10,6))
+sns.scatterplot(x='education_years', y='salary', data = salary_data, color = 'blue')
+sns.regplot(x='education_years', y='salary', data = salary_data, scatter=False)
+plt.show()
